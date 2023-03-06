0649577a5dea79315841d84eb2bf1c19c9f54c99
EC2 Default User <ec2-user@ip-172-31-2-141.ap-northeast-1.compute.internal>
2023-03-06 05:24:57 +0000

    edited de.sh

--- b.sh
+++ b.sh
@@ -4,5 +4,9 @@
 read -p "Enter image name: " IMAGE_NAME
 read -p "Enter image version:" IMAGE_VERSION
 
+# Remove images 
+docker rmi $(docker images -q)
+
+
 # Build Docker image
 docker build -t "${IMAGE_NAME}:${IMAGE_VERSION}" .
--- de.sh
+++ de.sh
@@ -11,6 +11,6 @@ sudo docker stop $(docker ps -a -q)
 sudo docker rm $(docker ps -a -q)
 
 # Start new container with the latest image
-docker run -d --name $IMAGE_NAME -p 80:80 $IMAGE_VERSION
+sudo docker run -it -p 80:3000 $IMAGE_NAME:$IMAGE_VERSION
 sudo docker ps
 sudo docker ps -a
