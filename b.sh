13f93b9076342f7519a9ac870551289c560a2f34
chandrashekar21041997 <119240540+chandrashekar21041997@users.noreply.github.com>
2023-03-08 16:47:24 +0530

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Define variables for image name and version
-read -p "Enter image name: " IMAGE_NAME
-read -p "Enter image version:" IMAGE_VERSION
+
 
 # Remove images 
 docker rmi $(docker images -q)
 
 
 # Build Docker image
-docker build -t "${IMAGE_NAME}:${IMAGE_VERSION}" .
+docker build -t react:v1 .
