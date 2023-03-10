1002d50d000d6edd81ce5fecd503ab1fbc94ce63
chandrashekar21041997 <119240540+chandrashekar21041997@users.noreply.github.com>
2023-03-10 16:51:20 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-# Define variables for image name and version
-IMAGE_NAME="my-image"
-IMAGE_VERSION="latest"
-
-# Build Docker image
-docker build -t "${IMAGE_NAME}:${IMAGE_VERSION}" .
+sudo npm run build &&
+sudo docker build -t react-app .
