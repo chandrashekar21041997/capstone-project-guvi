e1b9d8c2a37b7cdd3fdd7d9740c20697237dee26
chandrashekar21041997 <119240540+chandrashekar21041997@users.noreply.github.com>
2023-03-12 19:44:48 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo docker image rm -f chandrashekar117/dev:react-app
+sudo docker image rmi -f chandrashekar117/dev:react-app
+sudo docker rmi -f react-app:latest
 sudo  docker rmi -f node:latest
-sudo docker rmi -f react:latest
 sudo docker build -t react-app:latest .
